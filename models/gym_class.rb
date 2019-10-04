e0a819fc00a0551445628534ded523ea191d674e
Mark Payne <mjpayne8@gmail.com>
2019-10-04 21:23:46 +0100

    save, update and delete methodadded to GymClass class

--- models/gym_class.rb
+++ models/gym_class.rb
@@ -2,11 +2,38 @@ require_relative('../db/sql_runner')
 
 class GymClass
 
+  attr_accessor(:class_name, :class_date, :class_time)
+  attr_reader(:id)
+
   def initialize( options )
     @id = options['id'] if options['id']
     @class_name = options['class_name']
     @class_date = options['class_date']
     @class_time = options['class_time']
+  end
+
+  def save()
+    sql = "INSERT INTO gym_classes
+    (class_name, class_date, class_date)
+    VALUES
+    ($1,$2,$3,$4) RETURNING id"
+    values = [@class_name, @class_date, @class_time]
+    @id = SqlRunner.run(sql, values)[0]['id']
+  end
+
+  def update()
+    sql = "UPDATE gym_classes
+    SET (class_name, class_date, class_time) = ($1,$2,$3)
+    WHERE id = $4"
+    values = [@class_name, @class_date, @class_time, @id]
+    SqlRunner.run(sql,values)
+  end
+
+  def delete
+    sql = "DELETE FROM gym_classes
+    WHERE id = $1"
+    values = [@id]
+    SqlRunner.run(sql, values)
   end
 
 end
