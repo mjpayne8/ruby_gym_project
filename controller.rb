106d3a27558dc01670019427325ccd4aab32af33
Mark Payne <mjpayne8@gmail.com>
2019-10-05 19:07:38 +0100

    gym_controller added

--- controller.rb
+++ controller.rb
@@ -1,6 +1,7 @@
 require('sinatra')
 require('sinatra/contrib/all')
 require_relative('./controllers/member_controller')
+require_relative('./controllers/gym_class_controller')
 also_reload('../controllers/*')
 
 get '/' do
