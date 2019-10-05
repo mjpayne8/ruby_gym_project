3228e975638bc3906c2ccf3743824402d12460a4
Mark Payne <mjpayne8@gmail.com>
2019-10-05 16:18:52 +0100

    outline for member_controller.rb written

--- controllers/member_controller.rb
+++ controllers/member_controller.rb
@@ -3,7 +3,31 @@ require('sinatra/contrib/all')
 require_relative('../models/member')
 also_reload('../models/*')
 
-get '/member' do
+get '/members/new' do
+
+end
+
+post '/members' do
+
+end
+
+get '/members' do
   @members = Member.all()
   erb(:'./member/all_members')
+end
+
+get '/members/:id' do
+
+end
+
+get '/members/:id/edit' do
+
+end
+
+post 'members/:id' do
+
+end
+
+post 'members/:id/delete' do
+
 end
